330e8bc1779f2e4fd13c121bb23c9bfd681f359a
somq14 <som.q14@gmail.com>
2021-01-31 17:49:36 +0900

    dynamodbを生成し、APIからアクセス

--- apis/items/get_item/app.py
+++ apis/items/get_item/app.py
@@ -1,9 +1,38 @@
 import json
+import os
+import boto3
+
+dynamodb = boto3.resource(
+    "dynamodb", endpoint_url=os.getenv("DynamoDBEndpointUrl"))
+table = dynamodb.Table(os.getenv("DynamoDBTableName"))
 
 
 def lambda_handler(event, context):
-    print(event, context)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({})
-    }
+    item_id = event["pathParameters"]["item_id"]
+
+    try:
+        res = table.get_item(Key={
+            "PK": item_id,
+            "SK": item_id,
+        })
+
+        if 'Item' not in res:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({}),
+            }
+
+        item = res["Item"]
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "id": item["PK"],
+                "title": item["title"],
+            })
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "internal server error"})
+        }
--- apis/items/list_items/app.py
+++ apis/items/list_items/app.py
@@ -1,9 +1,33 @@
 import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Attr
+
+dynamodb = boto3.resource(
+    "dynamodb", endpoint_url=os.getenv("DynamoDBEndpointUrl"))
+table = dynamodb.Table(os.getenv("DynamoDBTableName"))
 
 
 def lambda_handler(event, context):
-    print(event, context)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({})
-    }
+    try:
+        res = table.scan(
+            Limit=100,
+            FilterExpression=Attr("title").exists())
+
+        items = list(map(lambda item: {
+            "id": item["PK"],
+            "title": item["title"]
+        }, res["Items"]))
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "items": items
+            })
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "internal server error"})
+        }
