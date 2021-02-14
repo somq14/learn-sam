b1225f2c28b746581c87bea6891cf274ea7e45f9
somq14 <som.q14@gmail.com>
2021-02-14 19:26:56 +0900

    lintエラー等を修正

--- apis/items/get_item/app.py
+++ apis/items/get_item/app.py
@@ -1,22 +1,30 @@
 import json
 import os
+from typing import Any, Dict
+
 import boto3
 
-dynamodb = boto3.resource(
-    "dynamodb", endpoint_url=os.getenv("DynamoDBEndpointUrl"))
-table = dynamodb.Table(os.getenv("DynamoDBTableName"))
+dynamoEndpointUrl = os.getenv("DynamoDBEndpointUrl")
+dynamoTableName = os.getenv("DynamoDBTableName")
+if dynamoEndpointUrl is None or dynamoTableName is None:
+    raise Exception("error")
+
+dynamodb = boto3.resource("dynamodb", endpoint_url=dynamoEndpointUrl)
+table = dynamodb.Table(dynamoTableName)
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     item_id = event["pathParameters"]["item_id"]
 
     try:
-        res = table.get_item(Key={
-            "PK": item_id,
-            "SK": item_id,
-        })
+        res = table.get_item(
+            Key={
+                "PK": item_id,
+                "SK": item_id,
+            }
+        )
 
-        if 'Item' not in res:
+        if "Item" not in res:
             return {
                 "statusCode": 404,
                 "body": json.dumps({}),
@@ -25,14 +33,16 @@ def lambda_handler(event, context):
         item = res["Item"]
         return {
             "statusCode": 200,
-            "body": json.dumps({
-                "id": item["PK"],
-                "title": item["title"],
-            })
+            "body": json.dumps(
+                {
+                    "id": item["PK"],
+                    "title": item["title"],
+                }
+            ),
         }
     except Exception as e:
         print(e)
         return {
             "statusCode": 500,
-            "body": json.dumps({"message": "internal server error"})
+            "body": json.dumps({"message": "internal server error"}),
         }
--- apis/items/list_items/app.py
+++ apis/items/list_items/app.py
@@ -1,33 +1,31 @@
 import json
 import os
+from typing import Any, Dict
+
 import boto3
 from boto3.dynamodb.conditions import Attr
 
-dynamodb = boto3.resource(
-    "dynamodb", endpoint_url=os.getenv("DynamoDBEndpointUrl"))
-table = dynamodb.Table(os.getenv("DynamoDBTableName"))
+dynamoEndpointUrl = os.getenv("DynamoDBEndpointUrl")
+dynamoTableName = os.getenv("DynamoDBTableName")
+if dynamoEndpointUrl is None or dynamoTableName is None:
+    raise Exception("error")
+
+dynamodb = boto3.resource("dynamodb", endpoint_url=dynamoEndpointUrl)
+table = dynamodb.Table(dynamoTableName)
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     try:
-        res = table.scan(
-            Limit=100,
-            FilterExpression=Attr("title").exists())
+        res = table.scan(Limit=100, FilterExpression=Attr("title").exists())
 
-        items = list(map(lambda item: {
-            "id": item["PK"],
-            "title": item["title"]
-        }, res["Items"]))
+        items = list(
+            map(lambda item: {"id": item["PK"], "title": item["title"]}, res["Items"])
+        )
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "items": items
-            })
-        }
+        return {"statusCode": 200, "body": json.dumps({"items": items})}
     except Exception as e:
         print(e)
         return {
             "statusCode": 500,
-            "body": json.dumps({"message": "internal server error"})
+            "body": json.dumps({"message": "internal server error"}),
         }
