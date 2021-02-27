fc10d8069fd51d9eb26387fcf68816e5dda8048c
somq14 <som.q14@gmail.com>
2021-02-27 16:43:49 +0900

    dynamo

---
+++ dynamodb/local/init_table.py
@@ -0,0 +1,43 @@
+import boto3
+
+endpoint_url = "http://dynamodb:8000"
+table_name_suffix = "-local"
+
+dynamodb = boto3.resource("dynamodb", endpoint_url=endpoint_url)
+
+for table in dynamodb.tables.all():
+    table.delete()
+
+dynamodb.create_table(
+    TableName="User" + table_name_suffix,
+    AttributeDefinitions=[
+        {
+            "AttributeName": "UserId",
+            "AttributeType": "S",
+        },
+    ],
+    KeySchema=[
+        {
+            "AttributeName": "UserId",
+            "KeyType": "HASH",
+        },
+    ],
+    BillingMode="PAY_PER_REQUEST",
+)
+
+dynamodb.create_table(
+    TableName="Session" + table_name_suffix,
+    AttributeDefinitions=[
+        {
+            "AttributeName": "SessionId",
+            "AttributeType": "S",
+        },
+    ],
+    KeySchema=[
+        {
+            "AttributeName": "SessionId",
+            "KeyType": "HASH",
+        },
+    ],
+    BillingMode="PAY_PER_REQUEST",
+)
